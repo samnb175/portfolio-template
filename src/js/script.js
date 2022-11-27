81306414c55bf705e905c6ed64387278580b28a5
DeanNab175 <36784137+DeanNab175@users.noreply.github.com>
2022-11-27 17:58:03 +0100

    Develop (#11)
    * add more utilities class(width, viewport-height, font-weight and box-shadow), add image object-fit classes and secondary color
    
    * add button hover effect
    
    * remove duplicate font-weight variables
    
    * global button size
    
    * button sizing, button group and box shadow on buttons
    
    * compiled files
    
    * Create navbar and burger menu
    
    * card style and fix button group
    
    * navbar responsive
    
    * add all bootstrap sass components call
    
    * card styles
    
    * style card
    
    * compiled files
    
    * style card component
    
    * add records number block and testimonials and glide slider
    
    * merged feature hp-contents-styles
    
    Co-authored-by: samnb175 <104582343+samnb175@users.noreply.github.com>
    Co-authored-by: samnb175 <samirnabee@hotmail.com>

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,15 @@
 import App from './modules/app.js';
 import './modules/navbar-custom.js';
+import Glide from '@glidejs/glide';
 
 const app = new App();
+
+new Glide('#testimonials-glide',{
+    type: 'carousel',
+    startAt: 0,
+    perView: 3,
+    // gap: 15,
+    focusAt: 'center',
+    // wrapperWidth: 200
+    // slideWidth: 240
+}).mount();
